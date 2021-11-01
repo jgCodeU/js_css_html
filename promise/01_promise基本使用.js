f61aeb296f4555726ef6cb5d66cbf3cc7137c566
hujianguo1 <2224818693@qq.com>
2021-11-01 22:23:07 +0800

    promise基础

---
+++ promise/01_promise基本使用.js
@@ -0,0 +1,26 @@
+// function foo() {
+//     return new Promise((resovle, reject) => {
+
+//     })
+// }
+
+class promise {
+    constructor(callBack){
+        let foo = function() {
+
+        }
+    }
+}
+
+
+const promise = new Promise((resovle, reject) => {
+    // console.log('执行传入的函数')
+    
+})
+
+ 
+
+
+
+
+
---
+++ promise/02_resolve详解.js
@@ -0,0 +1,40 @@
+
+//给resolve传入不同的值
+//（1）普通的值或者对象：当前promise的状态变为fullfiled
+//（2）传入一个promise ：当前promise的状态由传入的promise状态决定，而且，传递的值也由传入的promise来决定
+//（3）传入一个对象，并且这个对象实现了then方法，那么这个then方法会被执行，且该then方法决定当前primise的状态及传出的值
+
+
+
+//（2）给resolve函数传入一个promise
+// const p1 = new Promise((resolve, reject) => {
+//     resolve('p1-resolve')
+// })
+// new Promise((resolve, reject) => {
+//     console.log('---------')
+//     resolve(p1)
+//     // reject()
+// }).then((res) => {
+//     console.log("res", res)
+// }, (err) => {
+//     console.log("err", err)
+// })
+
+// (3)传入一个对象，并且这个对象实现了then方法，那么这个then方法会被执行，且该then方法决定当前primise的状态及传出的值
+const obj = {
+    name:'zhangsan',
+    then: function() {
+        console.log('对象实现then方法')
+        // resolve('对象实现then方法resolve')
+        // reject('对象实现then方法reject')
+    }
+}
+new Promise((resolve, reject) => {
+    resolve(obj)
+}).then((res) => {
+    console.log(res)
+}, (err) => {
+    console.log(err)
+})
+
+
