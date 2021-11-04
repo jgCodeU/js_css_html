77a4416b88677d4f05040b25f6d398449f06695c
hujianguo1 <2224818693@qq.com>
2021-11-04 08:01:02 +0800

    手写Promise结构设计

--- promise/07_Promise_all().js
+++ promise/07_Promise_all().js
@@ -12,7 +12,7 @@ const p3 = new Promise((resolve, reject) => {
 })
 
 // 等所有的promise都resolve()之后再拿到结果,输出结果的顺序与all中数组元素的顺序相同
-//如果在所有promise都resolve之前，又一个promise被reject(),则按照reject()的结果执行
+//如果在所有promise都resolve之前，有一个promise被reject(),则按照reject()的结果执行
 Promise.all([p1, p2,p3]).then(res => {
     console.log(res)
 }).catch(err => {
---
+++ promise/11_手写promise_结构设计 copy.js
@@ -0,0 +1,30 @@
+const PROMISE_STATUS_PENDING = 'pending'
+const PROMISE_STATUS_FULFILLED = 'fulfilled'
+const PROMISE_STATUS_REJECTED = 'rejected'
+
+class MyPromise {
+    constructor(exeFn) {
+        this.status = PROMISE_STATUS_PENDING
+
+        const MyResolve = () => {
+            if(this.status === PROMISE_STATUS_PENDING){
+                this.status = PROMISE_STATUS_FULFILLED
+                console.log('执行MyResolve')
+            }
+        }
+        const MyReject = () => {
+            if(this.status === PROMISE_STATUS_PENDING){
+                this.status === PROMISE_STATUS_REJECTED
+                console.log('执行MyReject')
+            }
+        }
+        exeFn(MyResolve, MyReject)
+    }
+}
+
+
+const promise = new MyPromise((MyResolve, MyReject) => {
+    console.log('pending状态')
+    MyResolve()
+    MyReject()
+})
