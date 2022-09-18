b36c180018d0e6f8484d209ad1b7d17d6d1ab6af
hujianguo1 <2224818693@qq.com>
2022-09-19 01:29:16 +0800

    myInstanceOf完善

--- js/1-手写/006-实现instanceof/01-实现instanceof.js
+++ js/1-手写/006-实现instanceof/01-实现instanceof.js
@@ -1,9 +1,12 @@
-
 function myInstanceof(obj, fun) {
+    if(typeof obj !== 'object') {
+        return false
+    }
     let objProto = Object.getPrototypeOf(obj)
-    let funProto = fun.prototype
+    // console.log('objProto', objProto)
+    let funPrototype = fun.prototype 
     while(objProto) {
-        if(objProto === funProto) {
+        if(objProto === funPrototype) {
             return true
         } else {
             objProto = Object.getPrototypeOf(objProto)
@@ -17,4 +20,11 @@ myInstanceof(obj, Array)
 console.log('myInstanceof-1', myInstanceof(obj, Array))
 console.log('myInstanceof-2', myInstanceof(obj, Object))
 console.log('myInstanceof-3', myInstanceof(obj, Set))
+
+// 注意，原始类型使用 instanceOf 进行判断的时候会返回false
+// 例如：
+// console.log(null instanceof Object) // false 
+// console.log(1 instanceof Number) // false 
+// console.log('123' instanceof String) // false 
+// console.log(true instanceof Boolean) // false 
 
