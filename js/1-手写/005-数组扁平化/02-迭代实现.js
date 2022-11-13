f1a42cd63b67b3d2d2bc5859c1864f94e508ac25
hujianguo1 <2224818693@qq.com>
2022-11-13 18:01:22 +0800

    数组扁平化

--- js/1-手写/005-数组扁平化/02-迭代实现.js
+++ js/1-手写/005-数组扁平化/02-迭代实现.js
@@ -1,10 +1,11 @@
 function flatArr(arr) {
     if(!arr.length) return
     while(arr.some((item) => {
-        Array.isArray(item)
+        return Array.isArray(item)
     })) {
         arr = [].concat(...arr)
     }
+    return arr
 }
 console.log(flatArr([1,2,[2,3,[5,6,7],4],[1,2,[2,1,3],3]]))
 // [1, 2, 2, 3, 5, 6,7, 4, 1, 2, 2, 1,3, 3]
